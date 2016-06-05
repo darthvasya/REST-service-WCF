23a9318c7a26cd33aee7864554510bb9ff7d3191
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2016-06-05 21:19:16 +0300

    realy work example, work on hosting

---
+++ App1/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Net;
+using System.IO;
+
+namespace App1
+{
+    [Activity(Label = "App1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate {
+
+
+                button.Text = string.Format("{0} clicks!", count++);
+
+                WebRequest req = WebRequest.Create("http://localhost:55030/BookService.svc/BookS/");
+                WebResponse resp = req.GetResponse();
+                Stream stream = resp.GetResponseStream();
+                StreamReader sr = new StreamReader(stream);
+                string Out = sr.ReadToEnd();
+                sr.Close();
+                button.Text = string.Format("{0} clicks!"+ Out, count++);
+            };
+        }
+    }
+}
+
